cabf820cc57fcaf7f1d11810b67eeabe3258e978
zhangxiushan <626921381@qq.com>
2021-09-12 17:10:32 +0800

    2021.9.12

--- 连接mysql/main.go
+++ 连接mysql/main.go
@@ -10,6 +10,7 @@ import (
 type User struct {
 	Username string
 	Password string
+	id       int
 }
 
 //连接数据库所需要的参数
@@ -26,6 +27,94 @@ var DB *sql.DB
 
 func main() {
 	InitDB()
+	/*user1:=User{"zhang","123123",1}
+	InsertUser(user1)*/
+	//DeleteUser(user1)
+	/*user2:=User{"he","45678",1}
+	UpdateUser(user2)*/
+	SelectUserById(1)
+}
+
+//查询
+func SelectUserById(id int) {
+	var user User
+	err := DB.QueryRow("SELECT * FROM users WHERE id = ?", id).Scan(&user.Username, &user.Password, &user.id)
+	if err != nil {
+		fmt.Println("查询出错了", err)
+	}
+	fmt.Println(user)
+}
+func UpdateUser(user User) bool {
+	//开启事务
+	tx, err := DB.Begin()
+	if err != nil {
+		fmt.Println("tx fail")
+	}
+	//准备sql语句
+	stmt, err := tx.Prepare("UPDATE users SET name = ?, password = ? WHERE id = ?")
+	if err != nil {
+		fmt.Println("Prepare fail")
+		return false
+	}
+	//设置参数以及执行sql语句
+	res, err := stmt.Exec(user.Username, user.Password, user.id)
+	if err != nil {
+		fmt.Println("Exec fail")
+		return false
+	}
+	//提交事务
+	tx.Commit()
+	fmt.Println(res.LastInsertId())
+	return true
+}
+func DeleteUser(user User) bool {
+	//开启事务
+	tx, err := DB.Begin()
+	if err != nil {
+		fmt.Println("tx fail")
+	}
+	//准备sql语句
+	stmt, err := tx.Prepare("DELETE FROM users WHERE id = ?")
+	if err != nil {
+		fmt.Println("Prepare fail")
+		return false
+	}
+	//设置参数以及执行sql语句
+	res, err := stmt.Exec(user.id)
+	if err != nil {
+		fmt.Println("Exec fail")
+		return false
+	}
+	//提交事务
+	tx.Commit()
+	//获得上一个insert的id
+	fmt.Println(res.LastInsertId())
+	return true
+}
+func InsertUser(user User) bool {
+	//开启事务
+	tx, err := DB.Begin()
+	if err != nil {
+		fmt.Println("tx fail")
+		return false
+	}
+	//准备sql语句
+	stmt, err := tx.Prepare("INSERT INTO users (`name`, `password`,`id`) VALUES (?, ?,?)")
+	if err != nil {
+		fmt.Println("Prepare fail", err)
+		return false
+	}
+	//将参数传递到sql语句中并且执行
+	res, err := stmt.Exec(user.Username, user.Password, user.id)
+	if err != nil {
+		fmt.Println("InsertUser Exec fail err", err)
+		return false
+	}
+	//将事务提交
+	tx.Commit()
+	//获得上一个插入自增的id
+	fmt.Println(res.LastInsertId())
+	return true
 }
 
 //写一个初始化数据库的方法
